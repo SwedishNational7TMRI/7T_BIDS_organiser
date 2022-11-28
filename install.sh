81c9e87b0f88ee4d44ca7f10f7414d394417e0e2
Emil Ljungberg <emil.ljungberg@med.lu.se>
2022-11-28 15:45:33 +0100

    Install script for docker

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download docker containers
+
+# we'll be running the Docker containers as yourself, not as root:
+userID=$(id -u):$(id -g)
+
+###   Get docker images:   ###
+docker pull nipy/heudiconv:latest
+docker pull bids/validator:latest
