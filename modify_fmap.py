7ace36f0129897e3bd9a96ecc4fdc3c66cb2106b
TheoRum <theodor.rumetshofer@gmail.com>
2023-01-04 16:09:17 +0100

    remove B0FieldIdentifier field from the modify_fmap

--- modify_fmap.py
+++ modify_fmap.py
@@ -12,7 +12,7 @@ for f in files:
     file_intendedfor = gl(str_intendedfor)
     func_file = file_intendedfor[-1].split("/")[-3:]
     func_file = '/'+os.path.join(*func_file)
-    toadd = { "Units": "Hz", "IntendedFor": "bids::"+func_file, "B0FieldIdentifier": "b0map_fmap0"}
+    toadd = { "Units": "Hz", "IntendedFor": "bids::"+func_file}
 
     with open (f, 'r') as data:
         json_data=json.load(data)
